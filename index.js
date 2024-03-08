9b3f7f0e495d8c59db7f6c64adf9388beecee452
Chels93 <148895873+Chels93@users.noreply.github.com>
2024-03-08 10:14:11 -0500

    Update index.js content
    required express; created Express GET route located at the endpoint “/movies” returning JSON object containing data about top 10 movies; created GET route at endpoint “/” returning default textual response; express.static to serve “documentation.html” file from the public folder; used Morgan middleware library to log all requests; created error-handling middleware function that will log all application-level errors to the terminal

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+const express = require("express");
+const app = express ();
 
+//Log Requests (Morgan Middleware)
+let myLogger = (req, res, next) => {
+    console.log(req.url);
+    next();
+};
+
+let requestTime = (req, res, next) => {
+    req. requestTime = Date.now();
+    next ();
+};
+
+app.use(myLogger);
+app.use(requestTime);
+
+app.get('/', (req, res) => {
+    let responseText = 'Your Top Movies';
+    responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+    res.send(responseText);
+});
+
+app.get('/secreturl', (req, res) => {
+    let responseText = 'This is a secret url with super top-secret content.';
+    responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+    res.send(responseText);
+});
+
+app.get('documentation', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies' , (req, res) => {
+    res.json(topMovies);
+});
+
+//Error Handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
