502789e48dd6e608b4d108f026315b02e846439a
Chels93 <148895873+Chels93@users.noreply.github.com>
2024-03-12 16:40:40 -0400

    Update index.js
    Created Express functions pertaining to following list: 
    
    - Return a list of ALL movies to the user;
    - Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user;
    - Return data about a genre (description) by name/title (e.g., “Thriller”);
    - Return data about a director (bio, birth year, death year) by name;
    - Allow new users to register;
    - Allow users to update their user info (username);
    - Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later);
    - Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later);
    - Allow existing users to deregister (showing only a text that a user email has been removed—more on this later).

--- index.js
+++ index.js
@@ -75,8 +75,88 @@ app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
 });
 
+// Returns a list of all movies to the user
 app.get('/movies' , (req, res) => {
     res.json(topMovies);
+});
+
+// Returns data (description, genre, director, image URL) about a single movie by title
+app.get('/movies/:title', (req, res) => {
+    res.json(movies.find((movie) => 
+    { return movie.title === req.params.title }));
+});
+
+// Returns data about a genre by name/title
+app.get('/movies/genres/:genreName', (req, res) => {
+    res.json(movies.find((movie) => 
+    { return movie.genreName === req.params.genreName }));
+});
+
+// Returns data about a director (bio, birth year, death year) by name
+app.get('/movies/directors/:directorName', (req, res) => {
+    res.json(movies.find((movie) =>
+    { return movie.directorName === req.params.directorName }));
+});
+
+// Allows new users to register
+app.post('/users', (req, res) => {
+    let newUser = req.body;
+
+    if (!newUser.name) {
+        const message = 'Missing in request body.';
+        res.status(400).send(message);
+    } else {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).send(newUser);
+    }
+});
+
+// Allows users to update their user info (birthday)
+app.put('users/:userName', (req, res) => {
+    let user = users.find((user) => { return user.name === req.params.name });
+
+    if (user) {
+        user.birthday[req.params.birthday] = parseInt(req.params.birthday);
+        res.status(201).send('User ' + req.params.name + 'updated their birthday to ' + req.params.birthday);
+    } else {
+        res.status(404).send('User with the name ' + req.params.name + 'was not found.');
+    }
+});
+
+
+// Allows users to add a movie to their list of favorites
+app.post('/users/:userName/movies/:movieID', (req, res) => {
+    let newFavorite = req.body;
+
+    if (!newFavorite.name) {
+        const message = 'Missing name in request body';
+        res.status(400).send(message);
+    } else {
+        newFavorite.id = uuid.v4();
+        users.push(newFavorite);
+        res.status(201).send(newFavorite);
+    }
+});
+
+// Allows users to remove a movie from their list of favorites
+app.delete('/users/:userName/movies/:movieID', (req, res) => {
+    let user = users.find((user) => { return user.id === req.params.id });
+
+    if (user) {
+        users = users.filter ((obj) => { return obj.id !== req.params.id });
+        res.status(201).send('Movie ' + req.params.id + 'was deleted.');
+    }
+});
+
+// Allows existing users to deregister 
+app.delete('/users/:userName', (req, res) => {
+    let user = users.find((userName) => { return user.id === req.params.id});
+
+    if (user) {
+        users = users.filter((obj) => { return obj.id !== req.params.id });
+        res.status(201).send('User ' + req.params.id + 'was deleted.');
+    }
 });
 
 //Error Handling
