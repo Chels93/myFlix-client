d525cf9a4aec7f612f11fdbd1558bf5a2ad28b5e
Chels93 <148895873+Chels93@users.noreply.github.com>
2024-03-20 11:58:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -350,7 +350,3 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
-
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-});
