0e18d494daf0f443e64162fceeb83a3d4826a456
Chels93 <148895873+Chels93@users.noreply.github.com>
2024-04-05 13:16:26 -0400

    Update models.js
    updated module.exports

--- models.js
+++ models.js
@@ -15,6 +15,18 @@ let movieSchema = mongoose.Schema({
     imagePath: String,
     Featured: Boolean,
 });
+let directorSchema = mongoose.Schema({
+    _id:{ type: mongoose.Schema.Types.ObjectId, ref: 'directors', required: true},
+    Name:{type: String, required: true},
+    Bio:{type: String, required: true}, 
+    Birthyear: Date,
+    Deathyear: Date   
+});
+let genreSchema = mongoose.Schema({
+    _id:{ type: mongoose.Schema.Types.ObjectId, ref: 'genre', required: true},
+    Name:{type: String, required: true},
+    Description:{type: String, required: true} 
+});
 
 let userSchema = mongoose.Schema({
     userName: {type: String, required: true},
@@ -26,6 +38,10 @@ let userSchema = mongoose.Schema({
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+let Director = mongoose.model('Director', directorSchema)
+let Genre = mongoose.model('Genre', genreSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Director = Director;
+module.exports.Genre = Genre;
