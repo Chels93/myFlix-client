7f53100cad2190e3d933a239cc85cc923509d508
Chels93 <148895873+Chels93@users.noreply.github.com>
2024-04-26 15:04:58 -0400

    Create router-movies.js

---
+++ router-movies.js
@@ -0,0 +1,76 @@
+const passport = require("passport");
+const Models = require("./models.js");
+
+const Movies = Models.Movie;
+
+module.exports = (app) => {
+  // Returns a JSON object of all movies to the user
+  app.get(
+    "/movies",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      await Movies.find()
+        .then((movies) => {
+          res.status(201).json(movies);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send("Error: " + err);
+        });
+    }
+  );
+
+  // Returns data (description, genre, director, image URL) about a single movie by title
+  app.get(
+    "/movies/:Title",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      await Movies.findOne({ Title: req.params.Title })
+        .then((movie) => {
+          res.json(movie);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send("Error: " + err);
+        });
+    }
+  );
+
+  // Returns data about a genre by name/title
+  app.get(
+    "/movies/genre/:genreName",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      await Movies.findOne({ "Genre.Name": req.params.genreName })
+        .then((genre) => {
+          if (!genre) {
+            return res.status(404).send("Genre not found.");
+          }
+          res.status(200).json(genre);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send("Error: " + err);
+        });
+    }
+  );
+
+  // Returns data about a director (bio, birth year, death year) by name
+  app.get(
+    "/movies/director/:directorName",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      await Movies.findOne({ "Director.Name": req.params.directorName })
+        .then((director) => {
+          if (!director) {
+            return res.status(404).send("Director not found.");
+          }
+          res.status(200).json(director);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send("Error: " + err);
+        });
+    }
+  );
+};
