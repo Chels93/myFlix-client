d85a40a0afaed9a2d45351501a821802147fee04
Chels93 <148895873+Chels93@users.noreply.github.com>
2024-04-28 16:20:06 -0400

    Update index.js
    changed from only localhost for port 8080

--- index.js
+++ index.js
@@ -48,5 +48,7 @@ app.use((err, req, res, next) => {
 });
 
 // Listen on port
-app.listen(8080, () => console.log("Your app is listening on Port 8080."));
-
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
+});
