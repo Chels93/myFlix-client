b0003ded1f8e32e924834cc9ad73b5b6b8cf7b30
Chels93 <cacguff@gmail.com>
2024-05-07 16:59:42 -0400

    updated syntax

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,18 @@
-import { createRoot } from 'react-dom/client';
-import { MainView } from './components/main-view/main-view';
-
-import "./index.scss";
+import { createRoot } from "react-dom/client";
+import { MainView } from "./components/main-view/main-view";
 
 // Import statement to indicate need to bundle `./index.scss`
 import "./index.scss";
 
-// Main component 
+// Main component
 const MyFlixApplication = () => {
-    return (
-        <MainView />
-    );
+  return;
+  <MainView />;
 };
 
 // Finds root of app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-// Tells React to render app in root DOM element 
+// Tells React to render app in root DOM element
 root.render(<MyFlixApplication />);
