edcc9b001be3283d06a4e3458c8c75c3bf3f7e10
Chels93 <cacguff@gmail.com>
2024-05-07 16:09:41 -0400

    reformat and match views to parallel database

--- src/components/main-view/movie-view.jsx
+++ src/components/main-view/movie-view.jsx
@@ -1,30 +1,35 @@
 export const MovieView = ({ movie }) => {
-    return (
-        <div>
-            <div>
-                <img src={movie.image} />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Synopsis: </span>
-                <span>(movie.synopsis)</span>
-            </div>
-            <div>
-                <span>Poster Image: </span>
-                <span>(movie.image)</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>(movie.genre)</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.director}</span>
-            </div>
-            <button>Back</button>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <img src={movie.image} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Synopsis: </span>
+        <span>(movie.synopsis)</span>
+      </div>
+      <div>
+        <span>Poster Image: </span>
+        <span>(movie.image)</span>
+      </div>
+      <div>
+        <span>Release Year</span>
+        <span>(movie.year)</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>(movie.genre)</span>
+      </div>
+
+      <button>Back</button>
+    </div>
+  );
 };
