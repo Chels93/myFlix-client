747a7ad95968acc833dab3edb8ce6e0428df87cf
Chels93 <cacguff@gmail.com>
2024-05-08 21:32:34 -0400

    reconfigure to debug

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,40 @@
 import { useState } from "react";
-import { MovieCard } from "..movie-card/movie-card";
-import { MovieView } from "..movie-view/movie-view";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([
     {
       id: 1,
-      title: "Harry Potter and the Sorceror's Stone",
-      image: "imagepath.jpg",
+      title: "Harry Potter and the Sorceror`s Stone",
+      image: "https://i.ebayimg.com/images/g/nHQAAOSwQ8hhoMr~/s-l1600.jpg",
       director: "Chris Columbus",
     },
     {
       id: 2,
       title: "Inside Out",
-      image: "imagepath.jpg",
+      image:
+        "https://cdn.printerval.com/unsafe/960x960/asset.prtvstatic.com/2024/04/08/66138aa6e14a11.34079191.png",
       director: "Peter Docter",
     },
     {
       id: 3,
       title: "Bourne Identity",
-      image: "imagepath.jpg",
+      image: "https://i.ebayimg.com/images/g/A9UAAOSwiDFYNI7u/s-l1600.jpg",
       director: "Doug Liman",
     },
     {
       id: 4,
       title: "Love Actually",
-      image: "imagepath.jpg",
+      image:
+        "https://m.media-amazon.com/images/I/5199VZVD6KL._SX300_SY300_QL70_FMwebp_.jpg",
       director: "Richard Curtis",
     },
     {
       id: 5,
       title: "What A Girl Wants",
-      image: "imagepath.jpg",
+      image:
+        "https://m.media-amazon.com/images/I/51e0MZf+32L._AC_UF894,1000_QL80_.jpg",
       director: "Dennie Gordon",
     },
   ]);
@@ -40,9 +43,12 @@ export const MainView = () => {
 
   if (selectedMovie) {
     return (
-        <MovieView movie={selectedMovie} onBackClick={() => setSelectedBook(null)} />
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
     );
-}
+  }
 
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
@@ -54,7 +60,7 @@ export const MainView = () => {
         <MovieCard
           key={movie.id}
           movie={movie}
-          onClick={(newSelectedMovie) => {
+          onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
           }}
         />
