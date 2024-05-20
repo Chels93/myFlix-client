3a8159732b7fb544d81c1d563b46150f1a1312ad
Chels93 <cacguff@gmail.com>
2024-05-20 16:28:21 -0400

    reestablished component and index files
    index.html; index.jsx; index.scss; package.json; movie-card.jsx; movie-view.jsx; main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://mymoviesdb-6c5720b5bef1.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie._id,
+            imagePath: movie.imagePath,
+            title: movie.title,
+            synopsis: movie.synopsis,
+            year: movie.year,
+            genre: {
+              name: movie.genre?.name,
+              description: movie.genre?.description,
+            },
+            director: {
+              name: movie.director?.name,
+              bio: movie.director?.bio,
+              birthyear: movie.director?.birthyear,
+              deathyear: movie.director?.deathyear,
+            },
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
+  if (selectedMovie) {
+    return (
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
+    );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie._id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
