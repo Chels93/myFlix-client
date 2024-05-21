6dcc799dda18009f93c3a44cb4bbdc06ffd30c17
Chels93 <cacguff@gmail.com>
2024-05-21 10:48:22 -0400

    verify login info in LoginView

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 export const LoginView = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        const data = {
+            access: username,
+            secret: password
+        };
+
+        fetch("https://mymoviesdb-6c5720b5bef1.herokuapp.com/login"), {
+            method: "POST",
+            body: JSON.stringify(data)
+        });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Username: 
-                <input type="text" />
+                <input 
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)} 
+                />
             </label>
             <label>
                 Password: 
