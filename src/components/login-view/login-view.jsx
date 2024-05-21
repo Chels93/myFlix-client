a3d5738c4343ce14d5312a454be7123481c0cff8
Chels93 <cacguff@gmail.com>
2024-05-21 11:13:08 -0400

    syntax correction login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -18,40 +18,42 @@ export const LoginView = ({ onLoggedIn }) => {
         "content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    }).then((response) => response.json())
-    .then((data) => {
+    })
+      .then((response) => response.json())
+      .then((data) => {
         console.log("Login response: ", data);
         if (data.user) {
-            onLoggedIn(data.user, data.token);
+          onLoggedIn(data.user, data.token);
         } else {
-            alert("No such user");
+          alert("No such user");
         }
-    })
-    .catch((e) => {
+      })
+      .catch((e) => {
         alert("Something went wrong");
-    });
+      });
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
 };
