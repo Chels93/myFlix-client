00d1370308a6d26a9a0066131a64e216d3c4b7ef
Chels93 <cacguff@gmail.com>
2024-06-07 11:55:28 -0400

    birthyear and deathyear for director is optional

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -14,6 +14,8 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       <div>{movie.genre.description}</div>
       <div>{movie.director.name}</div>
       <div>{movie.director.bio}</div>
+      {movie.director.birthyear && <div>{movie.director.birthyear}</div>}
+      {movie.director.deathyear && <div>{movie.director.deathyear}</div>}
     </div>
   );
 };
