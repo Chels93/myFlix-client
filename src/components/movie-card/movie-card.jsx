e44f0adfbf57b56736dcf3cc83dcafe3f1b93f29
Chels93 <cacguff@gmail.com>
2024-06-07 13:12:36 -0400

    updated syntax movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,22 +2,32 @@ import PropTypes from "prop-types";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div>
-      <div>{movie.title}</div>
-      <div>{movie.synopsis}</div>
-      <div>{movie.year}</div>
-      <div>{movie.genre.name}</div>
-      <div>{movie.genre.description}</div>
-      <div>{movie.director.name}</div>
-      <div>{movie.director.bio}</div>
-      {movie.director.birthyear && <div>{movie.director.birthyear}</div>}
-      {movie.director.deathyear && <div>{movie.director.deathyear}</div>}
-      <img
-        src={movie.imagePath}
-        alt={movie.title}
-        onClick={() => onMovieClick(movie)}
-      />
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
     </div>
   );
 };
 
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    synopsis: PropTypes.string.isRequired,
+    year: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }).isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      birthyear: PropTypes.string,
+      deathyear: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
