d8a96a8738cd6f436ac97c8375022318e22698fa
Chels93 <cacguff@gmail.com>
2024-06-11 15:17:33 -0400

    added temporary border to the <Container>

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -2961,6 +2961,9 @@ var _containerDefault = parcelHelpers.interopDefault(_container);
 var _indexScss = require("./index.scss");
 const App = ()=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _containerDefault.default), {
+        style: {
+            border: "1px solid red"
+        },
         children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _mainView.MainView), {}, void 0, false, {
             fileName: "src/index.jsx",
             lineNumber: 10,
