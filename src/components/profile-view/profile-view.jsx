1e796051973d90f0b1cea952246c4581358d5575
Chels93 <cacguff@gmail.com>
2024-06-12 14:12:09 -0400

    populated update-user.jsx with form info from profile-view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -24,37 +24,7 @@ export function ProfileView({ movies, onUpdatedUserInfo }) {
     <div>
       <UserInfo name={user.username} email={user.email} />
      <FavoriteMovies favoriteMovieList={favoriteMovieList} />
-      <Form className="profile-form" onSubmit={(e) => handleSubmit(e)}>
-        <h2>Want to change some info?</h2>
-
-        <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          defaultValue={user.username}
-          onChange={(e) => handleUpdate(e)}
-        />
-
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          defaultValue={user.password}
-          onChange={(e) => handleUpdate(e)}
-        />
-
-        <label>Email Address</label>
-        <input
-          type="email"
-          name="email"
-          defaultValue={user.email}
-          onChange={(e) => handleUpdate(e.target.value)}
-        />
-
-        <Button variant="primary" type="submit">
-          Update
-        </Button>
-      </Form>
+      
     </div>
   );
 }
