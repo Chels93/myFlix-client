3368723c4dd4713f96fd7926c360c39b36f16664
Chels93 <cacguff@gmail.com>
2024-06-12 15:36:55 -0400

    implemented grid in favorite-movies

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -3,23 +3,28 @@ import { Link } from "react-router-dom";
 
 function FavoriteMovies({ favoriteMovieList }) {
   return (
-    <div>
-      <h2>Favorite Movies</h2>
-      {favoriteMovieList.map((movies) => {
-        return (
-          <div key={movies._id}>
-            <img src={movies.imagePath} />
-            <Link to={`/movies${movies._id}`}>
-              <h4>{movies.title}</h4>
-            </Link>
-            <Button variant="secondary" onClick={() => removeFav(movies._id)}>
-              Remove From List
-            </Button>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <Row>
+        <Col xs={12}>
+          <h2>Favorite Movies</h2>
+        </Col>
+      </Row>
+      <Row>
+        {favoriteMovieList.map((movies) => {
+          return (
+            <Col xs={12} md={6} lg={3} key={movies._id}>
+              <img src={movies.imagePath} />
+              <Link to={`/movies${movies._id}`}>
+                <h4>{movies.title}</h4>
+              </Link>
+              <Button variant="secondary" onClick={() => removeFav(movies._id)}>
+                Remove From List
+              </Button>
+            </Col>
+          );
+        })}
+      </Row>
+    </>
   );
 }
-
 export default FavoriteMovies;
