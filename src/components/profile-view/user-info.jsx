1786eb3e4c4d68356837947c7244ee9ffb6086f8
Chels93 <cacguff@gmail.com>
2024-06-12 15:07:26 -0400

    format user-info

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function UserInfo ({ email, name }) {
+function UserInfo({ email, name }) {
   return (
     <>
-    <h4>Your Info</h4>
+      <h4>Your Info</h4>
       <p>User: {name}</p>
       <p>Email: {email}</p>
     </>
