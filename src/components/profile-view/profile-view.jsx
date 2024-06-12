518783e17cea895580383758ef6a848c43bb193d
Chels93 <cacguff@gmail.com>
2024-06-12 14:25:08 -0400

    updated imports in profile-view to reflect new changes

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
-import "./profile-view.scss";
-import axios from "axios";
+import { Container, Col, Row, Card } from "react-bootstrap";
 import UserInfo from "./user-info";
 import FavoriteMovies from "./favorite-movies";
 import UpdateUser from "./update-user";
+import "./profile-view.scss";
 
 export function ProfileView({ movies, onUpdatedUserInfo }) {
   const [user, setUser] = useState({});
